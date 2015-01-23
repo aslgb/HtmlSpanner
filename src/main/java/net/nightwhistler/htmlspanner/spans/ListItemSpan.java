3e03967f1341dcc7ad43defb71a17c509cb60836
aslgb <aslgb>
2015-01-23 17:02:41 +0300

    measure leading margin when drawLeadingMargin

--- src/main/java/net/nightwhistler/htmlspanner/spans/ListItemSpan.java
+++ src/main/java/net/nightwhistler/htmlspanner/spans/ListItemSpan.java
@@ -21,7 +21,6 @@ import android.graphics.Paint;
 import android.text.Layout;
 import android.text.Spanned;
 import android.text.style.LeadingMarginSpan;
-import net.nightwhistler.htmlspanner.HtmlSpanner;
 
 /**
  * This class is similar to Android's BulletSpan with the following differences:
@@ -33,14 +32,10 @@ import net.nightwhistler.htmlspanner.HtmlSpanner;
  *
  * - Also supports ordered lists with numbers in front of the item
  */
+
 public class ListItemSpan implements LeadingMarginSpan {
     private final int mNumber;
-
-    private static final int BULLET_RADIUS = 3;
-    private static final int NUMBER_RADIUS = 5;
-
-    //Gap should be about 1em
-    public static final int STANDARD_GAP_WIDTH = HtmlSpanner.HORIZONTAL_EM_WIDTH;
+    private int mWidth;
 
     public ListItemSpan() {
         mNumber = -1;
@@ -51,11 +46,7 @@ public class ListItemSpan implements LeadingMarginSpan {
     }
 
     public int getLeadingMargin(boolean first) {
-        if (mNumber != -1) {
-            return 2 * NUMBER_RADIUS + STANDARD_GAP_WIDTH;
-        } else {
-            return 2 * BULLET_RADIUS + STANDARD_GAP_WIDTH;
-        }
+        return mWidth;
     }
 
     public void drawLeadingMargin(Canvas c, Paint p, int x, int dir, int top,
@@ -63,14 +54,17 @@ public class ListItemSpan implements LeadingMarginSpan {
                                   boolean first, Layout l) {
         if (((Spanned) text).getSpanStart(this) == start) {
             Paint.Style style = p.getStyle();
-
             p.setStyle(Paint.Style.FILL);
 
+            String index;
             if (mNumber != -1) {
-                c.drawText(mNumber + ".", x + dir, baseline, p);
+                index = mNumber + ". ";
             } else {
-                c.drawText("\u2022", x + dir, baseline, p);
+                index = "\u2022 "; // bullet symbol
             }
+
+            mWidth = Math.round(p.measureText(index, 0, index.length()));
+            c.drawText(index, x + dir, baseline, p);
 
             p.setStyle(style);
         }
